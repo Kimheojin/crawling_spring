e939f261902aa94905bcf76f3c3d4b1e53484dd9
jin <hurjin1109@naver.com>
2025-07-21 23:13:54 +0900

    INIT

---
+++ src/test/java/HeoJin/crawling_spring/CrawlingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package HeoJin.crawling_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
