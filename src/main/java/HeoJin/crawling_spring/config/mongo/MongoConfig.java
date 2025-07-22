cc22766067818cc0d40473fa43a5f56e982e5384
jin <hurjin1109@naver.com>
2025-07-22 21:43:51 +0900

    FEAT : mongo template 관련 config 코드 추가 + 기본 동작 테스트 코드 추가

---
+++ src/main/java/HeoJin/crawling_spring/config/mongo/MongoConfig.java
@@ -0,0 +1,38 @@
+package HeoJin.crawling_spring.config.mongo;
+
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+//    https://docs.spring.io/spring-data/mongodb/reference/mongodb/template-config.html
+
+    // 설정 안해도 자동으로 들어가는 듯
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+    // db 명 지정
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Bean
+    public MongoClient mongoClient(){
+        return MongoClients.create(mongoUri);
+    }
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(){
+        return new SimpleMongoClientDatabaseFactory(mongoClient(), databaseName);
+    }
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory){
+        return new MongoTemplate(mongoDatabaseFactory);
+    }
+
+}
---
+++ src/test/java/HeoJin/crawling_spring/config/mongo/MongoConfigTest.java
@@ -0,0 +1,203 @@
+package HeoJin.crawling_spring.config.mongo;
+
+import HeoJin.crawling_spring.entity.Recipe;
+import HeoJin.crawling_spring.entity.Ingredient;
+import HeoJin.crawling_spring.entity.CookingOrder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+// 실제 db 사용 X
+@DataMongoTest
+// uri 는 자동으로 들어가는듯
+@TestPropertySource(properties = {
+        "spring.data.mongodb.database=test_db"
+})
+class MongoConfigTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    void setUp() {
+        // 테스트 전 컬렉션클래스로 DB 초기화
+        mongoTemplate.dropCollection(Recipe.class);
+    }
+
+    @Test
+    @DisplayName("빈 생성 관련 정상 동작 테스트")
+    void test1() {
+        // given + when + then
+        assertThat(mongoTemplate).isNotNull();
+        assertThat(mongoTemplate.getCollectionNames()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("저장 관련 정상 동작 테스트")
+    void test2() {
+        // given
+        Recipe recipe = Recipe.builder()
+                .recipeName("김치찌개")
+                .cookingTime(30L)
+                .sourceUrl("http://example.com/recipe/1")
+                .siteIndex("1")
+                .crawledAt(LocalDateTime.now())
+                .build();
+
+        // when
+        Recipe savedRecipe = mongoTemplate.save(recipe);
+
+        // then
+        assertThat(savedRecipe.getId()).isNotNull();
+        assertThat(savedRecipe.getRecipeName()).isEqualTo("김치찌개");
+        assertThat(savedRecipe.getCookingTime()).isEqualTo(30L);
+        assertThat(savedRecipe.getSourceUrl()).isEqualTo("http://example.com/recipe/1");
+        assertThat(savedRecipe.getSiteIndex()).isEqualTo("1");
+        assertThat(savedRecipe.getCrawledAt()).isNotNull();
+        
+        // builder.default 빈 리스트로 들어가는지
+        assertThat(savedRecipe.getIngredientList().isEmpty()).isTrue();
+        assertThat(savedRecipe.getCookingOrderList().isEmpty()).isTrue();
+
+    }
+
+    @Test
+    @DisplayName("ingredientList + cookingOrderList 추가 entity 저장 관련 테스트")
+    void test3() {
+        // given
+        Ingredient ingredient1 = Ingredient.builder()
+                .ingredient("김치")
+                .quantity("200g")
+                .build();
+
+        Ingredient ingredient2 = Ingredient.builder()
+                .ingredient("돼지고기")
+                .quantity("100g")
+                .build();
+
+        CookingOrder order1 = CookingOrder.builder()
+                .step(1)
+                .instruction("김치를 볶는다")
+                .build();
+
+        CookingOrder order2 = CookingOrder.builder()
+                .step(2)
+                .instruction("돼지고기를 넣고 볶는다")
+                .build();
+
+        Recipe recipe = Recipe.builder()
+                .recipeName("김치찌개")
+                .cookingTime(30L)
+                .sourceUrl("http://example.com/recipe/1")
+                .siteIndex("site1")
+                .crawledAt(LocalDateTime.now())
+                .build();
+
+        recipe.getIngredientList().add(ingredient1);
+        recipe.getIngredientList().add(ingredient2);
+        recipe.getCookingOrderList().add(order1);
+        recipe.getCookingOrderList().add(order2);
+
+        // when
+        Recipe savedRecipe = mongoTemplate.save(recipe);
+
+        // then
+        assertThat(savedRecipe.getId()).isNotNull();
+        assertThat(savedRecipe.getIngredientList()).hasSize(2);
+        assertThat(savedRecipe.getCookingOrderList()).hasSize(2);
+        assertThat(savedRecipe.getIngredientList().get(0).getIngredient()).isEqualTo("김치");
+        assertThat(savedRecipe.getIngredientList().get(0).getQuantity()).isEqualTo("200g");
+        assertThat(savedRecipe.getCookingOrderList().get(0).getStep()).isEqualTo(1);
+        assertThat(savedRecipe.getCookingOrderList().get(0).getInstruction()).isEqualTo("김치를 볶는다");
+    }
+
+    @Test
+    @DisplayName("id를 통한 조회 관련 테스트")
+    void test4() {
+        // given
+        Recipe recipe = Recipe.builder()
+                .recipeName("된장찌개")
+                .cookingTime(25L)
+                .sourceUrl("http://example.com/recipe/2")
+                .siteIndex("site2")
+                .crawledAt(LocalDateTime.now())
+                .build();
+
+        Recipe savedRecipe = mongoTemplate.save(recipe);
+
+        // when
+        Recipe foundRecipe = mongoTemplate.findById(savedRecipe.getId(), Recipe.class);
+
+        // then
+        assertThat(foundRecipe).isNotNull();
+        assertThat(foundRecipe.getRecipeName()).isEqualTo("된장찌개");
+        assertThat(foundRecipe.getId()).isEqualTo(savedRecipe.getId());
+    }
+
+    @Test
+    @DisplayName("레시피 이름을 통한 조회 관련 테스트")
+    void test5() {
+        // given
+        Recipe recipe1 = Recipe.builder()
+                .recipeName("김치찌개")
+                .cookingTime(30L)
+                .sourceUrl("http://example.com/recipe/1")
+                .siteIndex("site1")
+                .crawledAt(LocalDateTime.now())
+                .build();
+
+        Recipe recipe2 = Recipe.builder()
+                .recipeName("된장찌개")
+                .cookingTime(25L)
+                .sourceUrl("http://example.com/recipe/2")
+                .siteIndex("site2")
+                .crawledAt(LocalDateTime.now())
+                .build();
+
+        mongoTemplate.save(recipe1);
+        mongoTemplate.save(recipe2);
+
+        // when
+        Query query = new Query(Criteria.where("recipeName").is("김치찌개"));
+        List<Recipe> foundRecipes = mongoTemplate.find(query, Recipe.class);
+
+        // then
+        assertThat(foundRecipes).hasSize(1);
+        assertThat(foundRecipes.get(0).getRecipeName()).isEqualTo("김치찌개");
+    }
+
+    @Test
+    @DisplayName("레시피 삭제 관련 테스트")
+    void test6() {
+        // given
+        Recipe recipe = Recipe.builder()
+                .recipeName("김치찌개")
+                .cookingTime(30L)
+                .sourceUrl("http://example.com/recipe/1")
+                .siteIndex("site1")
+                .crawledAt(LocalDateTime.now())
+                .build();
+
+        Recipe savedRecipe = mongoTemplate.save(recipe);
+
+        // when
+        mongoTemplate.remove(savedRecipe);
+
+        // then
+        Recipe foundRecipe = mongoTemplate.findById(savedRecipe.getId(), Recipe.class);
+        assertThat(foundRecipe).isNull();
+    }
+
+}
