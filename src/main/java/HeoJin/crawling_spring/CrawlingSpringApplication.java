8745b8644bebef46e81930cbc27f4a9e1db8ec67
jin <hurjin1109@naver.com>
2025-07-30 16:49:48 +0900

    REFACTOR : enable 제거 - application.yml, config 수정에 따른

--- src/main/java/HeoJin/crawling_spring/CrawlingSpringApplication.java
+++ src/main/java/HeoJin/crawling_spring/CrawlingSpringApplication.java
@@ -1,12 +1,9 @@
 package HeoJin.crawling_spring;
 
-import HeoJin.crawling_spring.config.RecipeUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RecipeUrlConfig.class)
 public class CrawlingSpringApplication {
 
 	public static void main(String[] args) {
